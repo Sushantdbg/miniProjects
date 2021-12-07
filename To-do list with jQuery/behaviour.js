fd00473c6a7d08ce0d8353c395cadeb4c585af2a
Sushant Kumar <86154075+Sushantdbg@users.noreply.github.com>
2021-12-07 18:02:57 +0530

    Updates with clear all and better remove button

--- To-do list with jQuery/behaviour.js
+++ To-do list with jQuery/behaviour.js
@@ -3,12 +3,24 @@ $(function() {
     var val = $("input").val();
     if (val !== '') {
       var elem = $("<li></li>").text(val);
-      $(elem).append("<button class='rem'>X</button>");
+      $(elem).append("<button class='rem'>❌</button>");
       $("#mylist").append(elem);
       $("input").val("");
-      $(".rem").on("click", function() {
-        $(this).parent().remove();
-      });
+    } else {
+      alert('Please enter something');
+    }
+  });
+
+  $("#mylist").on('click', 'button.rem', function() {
+    $(this).parent().remove();
+  });
+
+  $("#clearAll").click(function() {
+    let listItems = $('#mylist li');
+    if (listItems.length) {
+      listItems.remove();
+    } else {
+      alert('No items in To-Do-List.');
     }
   });
 });
