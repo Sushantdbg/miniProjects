bdc7bac90a491247b5de80e946b22ff9ccca0760
Sushant Kumar <86154075+Sushantdbg@users.noreply.github.com>
2021-12-18 18:50:06 +0530

    patched issues & added some explaination comments

--- SIMON GAME/game.js
+++ SIMON GAME/game.js
@@ -1,80 +1,74 @@
-var buttonColours = ["red", "blue", "green", "yellow"];
+//Before starting we have to know about how exactle does SIMON game works
+//In the game the user have to repeat the pattern of pressess randomly generated
+//by the sysetem
+var buttonColours = ["red", "blue", "green", "yellow"];//so to have a random output made a array
 var gamePattern = [];
 var userClickedPattern = [];
+var started = false;
 var level = 0;
-$(".btn").click(function() {
-  //2. Inside the handler, create a new variable called userChosenColour to store the id of the button that got clicked.
+//abive we initialised the required vars and arrays
+$(document).keypress(function() {//function to detect keypress
+  if (!started) {
+    $("#level-title").text("Level " + level);//change h1 to show current level
+    nextSequence();
+    started = true;
+  }
+});
+
+$(".btn").click(function() {//detect click on buttons
   var userChosenColour = $(this).attr("id");
-  //4. Add the contents of the variable userChosenColour created in step 2 to the end of this new userClickedPattern
-  userClickedPattern.push(userChosenColour);
+  userClickedPattern.push(userChosenColour);//push color to the user patern array
   playSound(userChosenColour);
   animatePress(userChosenColour);
-  //console.log(userClickedPattern);
-  checkAnswer(userChosenColour);
-
+  //play and animate simultaneously
+  checkAnswer(userClickedPattern.length - 1);//caling the check method to compare user and system pattern
 });
 
-document.addEventListener("keypress", function(event) {
-  if (level === 0) {
-    $("h1").html("Press A Key to Start");
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {//to check/compare latest input
+    console.log("success");
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function() {
+        nextSequence();
+      }, 1000);
+    }
+  } else {//when the user input in incorrect
+    console.log("wrong");
+    playSound("wrong");
+    $("body").addClass("game-over");
+    setTimeout(function() {
+      $("body").removeClass("game-over");
+    }, 200);
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+    startOver();//calling the startover method as user's game is over
   }
-  nextSequence();
-  buttonAnimation(event.key);
-});
+}
 
-function nextSequence() {
-  var randomNumber = Math.floor((Math.random() * 4));
+function nextSequence() {//this whole function is to create a new sequence / upgrade to next level
+  userClickedPattern = [];
+  level++;
+  $("#level-title").text("Level " + level);
+  var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
   playSound(randomChosenColour);
-  level++;
-  if (level > 0) {
-    $("h1").html("Level " + level);
-  }
-  checkAnswer(randomNumber);
 }
 
-function playSound(name) {
+function playSound(name) {//playing sound wrt to color of the key pressed
   var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
 }
 
-function animatePress(currentColor) {
+function animatePress(currentColor) {//animation wrt color of keypress
   $("#" + currentColor).addClass("pressed");
   setTimeout(function() {
     $("#" + currentColor).removeClass("pressed");
   }, 100);
 }
-//I broke something here BUT WHAT ??
-function checkAnswer(currentLevel) {
-  //3. Write an if statement inside checkAnswer() to check if the most recent user answer is the same as the game pattern. If so then log "success", otherwise log "wrong".
-  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
-    console.log("success");
 
-    //4. If the user got the most recent answer right in step 3, then check that they have finished their sequence with another if statement.
-    if (userClickedPattern.length === gamePattern.length) {
-      //5. Call nextSequence() after a 1000 millisecond delay.
-      setTimeout(function() {
-        nextSequence();
-      }, 1000);
-    }
-  } else {
-    console.log("wrong");
-    var audio2 = new Audio("sounds/wrong.mp3");
-    audio2.play();
-
-    $("body").addClass("game-over");
-    setTimeout(function() {
-      $("body").removeClass("game-over");
-    }, 1000);
-      $("h1").html("Game Over, Press Any Key to Restart");
-      startOver();
-  }
-}
-
-function startOver(){
-  level=0;
-  gamePattern=[];
-
+function startOver() {// resets everthing to start/initial state
+  level = 0;
+  gamePattern = [];
+  started = false;
 }
