4eded139d8288cd345d5933e199aed9e2be363db
Sushant Kumar <86154075+Sushantdbg@users.noreply.github.com>
2021-12-30 18:16:10 +0530

    added dynamic form to take input from user

--- WeatherAppWithAPI/server.js
+++ WeatherAppWithAPI/server.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const https = require("https"); //a native node module so doesn't need a npm install to work
 const app = express();
-
+const bodyParser=require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+})
+app.post("/", function(req, res){
 
+
+  //
+   const query=req.body.cityName;
+  // const unit="metric";
   //here below the url requires a strict formatting i.e. "https://"
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=Madhubani&appid=4b2aac5b4ba17b702c0f6787f04822fe&units=metric";
+  const url = "https://api.openweathermap.org/data/2.5/weather?q="+"query"+"&appid=4b2aac5b4ba17b702c0f6787f04822fe&units=metric";
   https.get(url, function(response) { // callback function
     // console.log(response);
     console.log(response.statusCode); //here we can use different type of specifiers
@@ -26,14 +34,14 @@ app.get("/", function(req, res) {
       // console.log(JSON.stringify(object));
       const imageURL = "https://openweathermap.org/img/wn/"+icon+"@2x.png"
       res.write("<h1><p> The weather here currently is "+weatherDescription+"</p></h1>");
-      res.write("<h1> The temperature in Madhubani is "+temp+" degree Celcius "+"and it feels like "+ feelsLike+ "</h1>");
+      res.write("<h1> The temperature in " +query+" is "+temp+" degree Celcius "+"and it feels like "+ feelsLike+ "</h1>");
       res.write("<img src="+imageURL+">");
       res.send()
     })
 
   })
-  // res.send("Server is up n Running");
 })
+
 
 app.listen(3000, function() {
   console.log(" Server is up and Running on port 3000");
