678ee879eebe7a8b360751a8abbc70005318a898
Sushant Kumar <86154075+Sushantdbg@users.noreply.github.com>
2021-12-30 16:41:17 +0530

    added documentation for the first step

--- WeatherAppWithAPI/server.js
+++ WeatherAppWithAPI/server.js
@@ -1,16 +1,14 @@
 const express = require("express");
-const https = require("https");
+const https = require("https"); //a native node module so doesn't need a npm install to work
 const app = express();
 
 app.get("/", function(req,res){
 
+  //here below the url requires a strict formatting i.e. "https://"
   const url = "https://api.openweathermap.org/data/2.5/weather?q=Madhubani&appid=4b2aac5b4ba17b702c0f6787f04822fe&units=metric";
-  https.get(url, function(response){
+  https.get(url, function(response){// callback function
     console.log(response);
   })
-
-
-
   // res.send("Server is up n Running");
 })
 
