789589ea16c47657176ae35de809a9d49f91d556
Sushant Kumar <86154075+Sushantdbg@users.noreply.github.com>
2022-02-21 21:22:14 +0530

    improve; formatting remaining

--- personalDigitalBusinessCard/src/Footer.js
+++ personalDigitalBusinessCard/src/Footer.js
@@ -3,9 +3,9 @@ import React from "react";
 export default function Footer(){
   return(
     <div className="footer--all">
-    <button type="button" className="btn btn-light"><img src={process.env.PUBLIC_URL + '/twitter.png'} className="footer--img" /></button>
-    <button type="button" className="btn btn-light"><img src={process.env.PUBLIC_URL + '/github-sign.png'} className="footer--img" /></button>
-    <button type="button" className="btn btn-light"><img src={process.env.PUBLIC_URL + '/instagram.png'} className="footer--img" /></button>
+    <img src={process.env.PUBLIC_URL + '/twitter.png'} className="footer--img" />
+    <img src={process.env.PUBLIC_URL + '/github-sign.png'} className="footer--img" />
+    <img src={process.env.PUBLIC_URL + '/instagram.png'} className="footer--img" />
     </div>
   )
 }
--- personalDigitalBusinessCard/src/MainContent.js
+++ personalDigitalBusinessCard/src/MainContent.js
@@ -3,11 +3,16 @@ import React from "react";
 export default function MainContent(){
   return(
     <div className="main--all">
-    <button type="button" className="btn btn-light"><img src={process.env.PUBLIC_URL + '/email.png'} className="main--img" />Email</button>
-    <button type="button" className="btn btn-light"><img src={process.env.PUBLIC_URL + '/linkedin.png'} className="main--img" />Linkedin</button>
+    <div className="personal--text">
+    <p className="name--text"> Sushant Kumar</p>
+    <p> Full Stack Developer</p>
+    <p> sushantdbg.in</p>
+    </div>
+    <button type="button" className="btn btn-light button button--email "><img src={process.env.PUBLIC_URL + '/email.png'} className="main--img" /><span className="email-text">Email</span></button>
+    <button type="button" className="btn btn-light button button--linkedin "><img src={process.env.PUBLIC_URL + '/linkedin.png'} className="main--img" /><span className="link-text">Linkedin</span></button>
     <h2>About</h2>
-    <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
-    <h2>interest</h2>
+    <p>I am a full stack with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+    <h2>Interest</h2>
     <p> Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
     </div>
   )
